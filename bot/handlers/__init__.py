4eb98c63e68adf965d58dae93aa0b36d936953a3
Ironiti <kirill.lvovich.2003@gmail.com>
2025-08-22 13:37:43 +0300

    .

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,6 +3,7 @@ from aiogram.enums.parse_mode import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
+from bot.handlers.poll_sender import poll_callback_router
 from bot.handlers.registration import registration_router
 from bot.handlers.feedback import feedback_router
 from bot.handlers.activation import activation_router
@@ -10,6 +11,7 @@ from bot.handlers.questions import questions_router
 from bot.handlers.admin import admin_router
 from bot.handlers.help import help_router
 from bot.handlers.utils import gif_router
+# from .questions import questions_router, questions_callbacks_router
 from config import BOT_API_KEY
 
 if not BOT_API_KEY:
@@ -21,7 +23,9 @@ bot = Bot(
 )
 dp = Dispatcher(storage=MemoryStorage())
 dp.include_router(registration_router)
+# dp.include_router(questions_callbacks_router)
 dp.include_router(feedback_router)
+dp.include_router(poll_callback_router)
 dp.include_router(activation_router)
 dp.include_router(questions_router)
 dp.include_router(admin_router)
